82fc9090017a7100d95f643fad687a8f52cf8f7e
Dima1409 <57713733+Dima1409@users.noreply.github.com>
2022-09-05 00:15:59 +0300

    timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,15 +8,19 @@ const refs = {
   field: document.querySelectorAll('.field'),
   fieldValue: document.querySelectorAll('.field .value'),
   fieldLabel: document.querySelectorAll('.field .label'),
+  days: document.querySelector('.value[data-days]'),
+  hours: document.querySelector('.value[data-hours]'),
+  minutes: document.querySelector('.value[data-minutes]'),
+  seconds: document.querySelector('.value[data-seconds]'),
   currentDate: new Date(),
+  second: 0,
 };
-console.log(refs.currentDate);
-// styles
+
+// styles ...........................................................
 refs.inputDate.classList.add('data_input');
 refs.btnStart.classList.add('data_start');
-
 refs.timer.classList.add('timer');
-////////////////////////////////////////////////////
+// ....................................................................
 refs.btnStart.disabled = true;
 
 const options = {
@@ -31,14 +35,56 @@ const options = {
       refs.btnStart.disabled = false;
       return;
     }
-    return (refs.btnStart.disabled = true);
+    refs.btnStart.disabled = true;
+    window.alert('Please choose a date in the future');
+    return;
   },
 };
-flatpickr(refs.inputDate, options);
 
-// refs.inputDate.addEventListener('input', checkDate);
-// function checkDate() {
-//   if (refs.currentDate.getTime() > selectedDates[0].getTime()) {
-//     refs.btnStart.disabled = !refs.btnStart.disabled;
-//   }
-// }
+//
+
+flatpickr(refs.inputDate, options);
+//
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+refs.btnStart.addEventListener('click', startTime);
+// .......................................................................................
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// .........................................................................................
+let intervalId = null;
+function startTime() {
+  refs.btnStart.disabled = true;
+  let result = Date.parse(refs.inputDate.value) - Date.parse(new Date());
+  if (result <= 0) {
+    document.body.style.backgroundColor = 'teal';
+    clearInterval(intervalId);
+    refs.btnStart.disabled = false;
+  }
+  convertMs(result).seconds -= 1;
+  intervalId = setInterval(startTime, 1000);
+
+  refs.days.textContent = addLeadingZero(convertMs(result).days);
+  refs.hours.textContent = addLeadingZero(convertMs(result).hours);
+  refs.minutes.textContent = addLeadingZero(convertMs(result).minutes);
+  refs.seconds.textContent = addLeadingZero(convertMs(result).seconds);
+}
