f96a88b147f75ad6b529c4cce0ae52daa778f7a4
Dima1409 <57713733+Dima1409@users.noreply.github.com>
2022-09-09 00:18:54 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,8 +7,16 @@ const refs = {
   amount: document.querySelector('input[name="amount"]'),
   btnSubmit: document.querySelector('button[type="submit"]'),
 };
+
 //---//
+refs.form.classList.add('form-promise');
+refs.delay.classList.add('form__input');
+refs.step.classList.add('form__input');
+refs.amount.classList.add('form__input');
+refs.btnSubmit.classList.add('color_btn');
+
 refs.btnSubmit.disabled = true;
+
 // активуємо кнопку Submit
 refs.form.addEventListener('input', submitActive);
 
@@ -17,32 +25,49 @@ function submitActive() {
     refs.btnSubmit.disabled = false;
   }
 }
-//---//
-refs.form.addEventListener('input', create);
-function create(e) {
-  console.log(refs.delay.value);
-  console.log(refs.step.value);
+
+// сабміт форми
+refs.form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  const values = {
+    amount: refs.amount.value,
+    step: refs.step.value,
+    delay: refs.delay.value,
+  };
+  console.log(values);
+
+  let delay = Number(values.delay);
+  let step = Number(values.step);
+  let amount = Number(values.amount);
+
+  for (let i = 1; i <= amount; i += 1) {
+    delay = delay + step;
+
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+
+  refs.form.reset();
+  refs.btnSubmit.disabled = true;
 }
-// виклик createPromise
-// refs.btnSubmit.addEventListener('submit', createPromise);
 
-// function createPromise(position, delay) {
-//   return new Promise((onSuccess, onError) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         onSuccess();
-//       } else {
-//         onError();
-//       }
-//     }, delay);
-//   });
-// }
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+// fn createPromise
+function createPromise(position, delay) {
+  return new Promise((onSuccess, onError) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        onSuccess({ position, delay });
+      } else {
+        onError({ position, delay });
+      }
+    }, delay);
+  });
+}
